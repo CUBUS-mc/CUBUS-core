b364493f4c40a8a02e04448e2122ac98cfeed89d
virus-rpi <n@bensanto.com>
2024-05-31 00:57:30 +0200

    chore: updated todos feat: added translations

--- shared/forms/cubeSetupForm.go
+++ shared/forms/cubeSetupForm.go
@@ -1,43 +1,45 @@
 package forms
 
-// TODO: Add translations
+import (
+	"CUBUS-core/shared/translation"
+)
 
 var CubeTypes = map[string]Option{
 	"queen": {
-		Label:       "Queen",
-		Description: "The Queen is the central cube in the CUBUS network. It is the C2 server. There can only be one Queen in a CUBUS subnet.",
+		Label:       translation.T("Queen"),
+		Description: translation.T("The Queen is the central cube in the CUBUS network. It is the C2 server. There can only be one Queen in a CUBUS subnet."),
 	},
 	"security": {
-		Label:       "Security",
-		Description: "The Security cube is responsible for the security of the CUBUS network.",
+		Label:       translation.T("Security"),
+		Description: translation.T("The Security cube is responsible for the security of the CUBUS network."),
 	},
 	"database": {
-		Label:       "Database",
-		Description: "The Database cube is responsible for storing data for the CUBUS network.",
+		Label:       translation.T("Database"),
+		Description: translation.T("The Database cube is responsible for storing data for the CUBUS network."),
 	},
 	"api": {
-		Label:       "API",
-		Description: "The API cube is responsible for providing an API for the CUBUS network.",
+		Label:       translation.T("API"),
+		Description: translation.T("The API cube is responsible for providing an API for the CUBUS network."),
 	},
 	"cubus-mod": {
-		Label:       "CUBUS Mod",
-		Description: "This is a special cube that runs in a Minecraft mod.",
+		Label:       translation.T("CUBUS Mod"),
+		Description: translation.T("This is a special cube that runs in a Minecraft mod."),
 	},
 	"discord-bot": {
-		Label:       "Discord Bot",
-		Description: "The Discord Bot cube is responsible for providing a Discord bot for the CUBUS network.",
+		Label:       translation.T("Discord Bot"),
+		Description: translation.T("The Discord Bot cube is responsible for providing a Discord bot for the CUBUS network."),
 	},
 	"web": {
-		Label:       "Web",
-		Description: "The Web cube is responsible for providing a web interface for the CUBUS network.",
+		Label:       translation.T("Web"),
+		Description: translation.T("The Web cube is responsible for providing a web interface for the CUBUS network."),
 	},
 	"drone": {
-		Label:       "Drone",
-		Description: "The Drone cubes are responsible for scanning, modifying and providing a proxy for the CUBUS network.",
+		Label:       translation.T("Drone"),
+		Description: translation.T("The Drone cubes are responsible for scanning, modifying and providing a proxy for the CUBUS network."),
 	},
 	"generic-worker": {
-		Label:       "Generic Worker",
-		Description: "The Generic Worker cube is a generic cube that can be used for any purpose by any other cube.",
+		Label:       translation.T("Generic Worker"),
+		Description: translation.T("The Generic Worker cube is a generic cube that can be used for any purpose by any other cube."),
 	},
 }
 
@@ -59,16 +61,16 @@ func GetCubeSetupForm() *Form {
 			"cubeLocation",
 			[]DisplayCondition{&AlwaysDisplay{}},
 			[]Validator{&ChoiceValidator{}},
-			"Please select the location of the cube",
-			"Cube Location",
+			translation.T("Please select the location of the cube"),
+			translation.T("Cube Location"),
 			map[string]Option{
 				"local": {
-					Label:       "Local",
-					Description: "Setup the cube on this device",
+					Label:       translation.T("Local"),
+					Description: translation.T("Setup the cube on this device"),
 				},
 				"remote": {
-					Label:       "Remote",
-					Description: "Setup the cube on a remote device",
+					Label:       translation.T("Remote"),
+					Description: translation.T("Setup the cube on a remote device"),
 				},
 			},
 			"",
@@ -77,16 +79,16 @@ func GetCubeSetupForm() *Form {
 			"cubeName",
 			[]DisplayCondition{&DisplayAfter{fieldId: "cubeLocation"}},
 			[]Validator{&NotEmptyValidator{}},
-			"Please enter the name of the cube",
-			"Cube Name",
+			translation.T("Please enter the name of the cube"),
+			translation.T("Cube Name"),
 			"",
 		),
 		NewMultipleChoiceField(
 			"cubeType",
 			[]DisplayCondition{&DisplayAfter{fieldId: "cubeName"}},
 			[]Validator{&ChoiceValidator{}},
-			"Please select the type of the cube",
-			"Cube Type",
+			translation.T("Please select the type of the cube"),
+			translation.T("Cube Type"),
 			CubeTypes,
 			"",
 		),
